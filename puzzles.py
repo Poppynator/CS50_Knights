e2cd6e8116bf140b7c88b0e1306975c9458a669a
Poppynator <popp.daniel16@icloud.com>
2023-09-25 08:53:34 +0200

    Last fixes

--- puzzles.py
+++ puzzles.py
@@ -9,13 +9,6 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
-"""
-know = And(
-    And(Or(AKnight,AKnave), Not(And(AKnight,AKnave))),
-    And(Or(BKnight,BKnave), Not(And(BKnight,BKnave))),
-    And(Or(CKnight,CKnave), Not(And(CKnight,CKnave)))
-)
-"""
 
 # Puzzle 0
 # A says "I am both a knight and a knave."
